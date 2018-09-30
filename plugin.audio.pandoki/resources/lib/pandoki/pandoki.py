f36c3ee80328da3ce8d7836373d9337e88e8d82a
rocky4546 <rcmccaslin@hotmail.com>
2018-09-29 20:51:51 -0500

    Updated logging

--- plugin.audio.pandoki/resources/lib/pandoki/pandoki.py
+++ plugin.audio.pandoki/resources/lib/pandoki/pandoki.py
@@ -331,7 +331,8 @@ class Pandoki(object):
     def Save(self, song):
         Log('def Save ', song, xbmc.LOGDEBUG)
         if (song['title'] == 'Advertisement') or (song.get('saved')) or (not song.get('cached', False)): return
-        if (Val('mode') in ('0', '3')) or ((Val('mode') == '2') and (song.get('voted') != 'up')): return
+        if (Val('mode') in ('0', '1')) or ((Val('mode') == '3') and (song.get('voted') != 'up')): return
+	return
         if (not self.Tag(song)): return
 
         tmp = "%s.%s" % (song['path'], song['encoding'])
